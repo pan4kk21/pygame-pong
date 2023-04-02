64524bfc8133c3bd0f3d9d64d9c057d60a25cab4
pan4kk21 <pan4kkpan4@gmail.com>
2023-04-02 22:55:22 +0300

    fixed the collision bug

--- main.py
+++ main.py
@@ -10,8 +10,22 @@ def ball_animation():
     if ball.left <= 0 or ball.right >= 1280:
         ball_restart()
 
-    if ball.colliderect(player) or ball.colliderect(opponent):
-        ball_speed_x *= -1
+    if ball.colliderect(player) and ball_speed_x > 0:
+        if abs(ball.right - player.left) < 10:
+            ball_speed_x *= -1
+        elif abs(ball.bottom - player.top) < 10 and ball_speed_y > 0:
+            ball_speed_y *= -1
+        elif abs(ball.top - player.bottom) < 10 and ball_speed_y < 0:
+            ball_speed_y *= -1
+
+    if ball.colliderect(opponent) and ball_speed_x < 0:
+        if abs(ball.left - opponent.right) < 10:
+            ball_speed_x *= -1
+        elif abs(ball.bottom - opponent.top) < 10 and ball_speed_y > 0:
+            ball_speed_y *= -1
+        elif abs(ball.top - opponent.bottom) < 10 and ball_speed_y < 0:
+            ball_speed_y *= -1
+
 
 def player_animation():
     player.y += player_speed
